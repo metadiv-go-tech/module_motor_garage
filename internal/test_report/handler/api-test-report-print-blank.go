a129cb6f517f5f35a769a40223d61cefe9237825
Nelson Lai <nelsonlai9503@gmail.com>
2024-12-14 10:38:35 +0800

    refactor test report print blank api

--- internal/test_report/handler/api-test-report-print-blank.go
+++ internal/test_report/handler/api-test-report-print-blank.go
@@ -9,6 +9,7 @@ import (
 	"github.com/metadiv-go-tech/metagin/v2"
 	"github.com/metadiv-go-tech/metagin/v2/base"
 	"github.com/metadiv-go-tech/module_motor_garage/config"
+	"github.com/metadiv-go-tech/module_motor_garage/internal/invoice/repo"
 	"github.com/metadiv-go-tech/module_motor_garage/internal/test_report/service"
 	"github.com/metadiv-go-tech/module_motor_garage/internal/util_client"
 )
@@ -16,10 +17,19 @@ import (
 var ApiTestReportPrintBlank = metagin.Get(
 	"getTestReportPrintBlank",
 	"Get Test Report Print Blank",
-	fmt.Sprintf("/api/%s/motor-garage/test-report/print/blank", config.SystemVersion),
-	func(ctx metagin.Context[base.Empty, base.Empty]) {
+	fmt.Sprintf("/api/%s/motor-garage/test-report/print/blank/:id", config.SystemVersion),
+	func(ctx metagin.Context[base.RequestPathId, base.Empty]) {
+
+		invoice := repo.MotorGarageInvoiceRepo.FindById(
+			ctx.DB().Preload("Vehicle", "Vehicle.Customer", "Vehicle.Customer.ContactPerson", "Services", "Discounts", "Products", "Inspect"),
+			ctx.Request().ID, ctx.WorkspaceId())
+		if invoice == nil {
+			ctx.Err(errors.New("invoice not found"))
+			return
+		}
+
 		pdf, err := util_client.HtmlToPdfClient.HtmlToPdf(gotenberg.HTML{
-			HTML: service.ReportService.GenerateBlankReport(),
+			HTML: service.ReportService.GenerateBlankReport(invoice, ctx.Locale()),
 		})
 		if err != nil {
 			ctx.ErrWithStatus(http.StatusInternalServerError, errors.New(err.Error()))
--- internal/test_report/service/ReportService.go
+++ internal/test_report/service/ReportService.go
@@ -343,19 +343,31 @@ func (s *reportService) GenerateReport(invoice *entity.MotorGarageInvoice, local
 	return html
 }
 
-func (s *reportService) GenerateBlankReport() string {
+func (s *reportService) GenerateBlankReport(invoice *entity.MotorGarageInvoice, locale string) string {
 
 	html := template.TestReportTemplate
 
-	html = strings.Replace(html, "{{invoice_number}}", "", -1)
-	html = strings.Replace(html, "{{invoice_date}}", "", -1)
+	html = strings.Replace(html, "{{invoice_number}}", fmt.Sprintf("#%d", invoice.ID), -1)
+	html = strings.Replace(html, "{{invoice_date}}", time.Unix(invoice.Date, 0).Format("02/01/2006"), -1)
 
-	html = strings.Replace(html, "{{customer_name}}", "", -1)
+	if invoice.Vehicle != nil && invoice.Vehicle.Customer != nil && invoice.Vehicle.Customer.ContactPerson != nil {
+		contactPerson := invoice.Vehicle.Customer.ContactPerson.ToDTO()
+		html = strings.Replace(html, "{{customer_name}}", contactPerson.FirstName+" "+contactPerson.LastName, -1)
+	} else {
+		html = strings.Replace(html, "{{customer_name}}", "-", -1)
+	}
 
-	html = strings.Replace(html, "{{vehicle_name}}", "", -1)
-	html = strings.Replace(html, "{{vehicle_year}}", "", -1)
-	html = strings.Replace(html, "{{vehicle_odometer}}", "", -1)
-	html = strings.Replace(html, "{{vehicle_registration}}", "", -1)
+	if invoice.Vehicle != nil {
+		html = strings.Replace(html, "{{vehicle_name}}", invoice.Vehicle.Name, -1)
+		html = strings.Replace(html, "{{vehicle_year}}", fmt.Sprintf("%d", invoice.Vehicle.Year), -1)
+		html = strings.Replace(html, "{{vehicle_odometer}}", fmt.Sprintf("%d", invoice.Vehicle.Odometer), -1)
+		vehicleDto := invoice.Vehicle.ToDTO(locale)
+		html = strings.Replace(html, "{{vehicle_registration}}", vehicleDto.Registration, -1)
+	} else {
+		html = strings.Replace(html, "{{vehicle_name}}", "-", -1)
+		html = strings.Replace(html, "{{vehicle_year}}", "-", -1)
+		html = strings.Replace(html, "{{vehicle_odometer}}", "-", -1)
+	}
 
 	html = strings.Replace(html, "{{tune}}", "", -1)
 	html = strings.Replace(html, "{{injector}}", "", -1)
